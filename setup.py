8294f17a8e6f561699ebee812adba9fefa9be1ea
Carl Meyer <carl@dirtcircle.com>
2009-07-02 16:16:04 -0400

    v0.2.0
    --HG--
    extra : convert_revision : carl%40dirtcircle.com-20090702201604-92x51wt6rvhzsm2i

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-model-utils',
-    version='0.1.0',
+    version='0.2.0',
     description='Django model mixins and utilities',
     long_description=open('README.txt').read(),
     author='Carl Meyer',
