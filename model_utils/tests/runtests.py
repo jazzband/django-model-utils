f04c200c09bc1b8c018970eacf631b7702b42383
Jannis Leidel <jannis@leidel.info>
2010-04-15 23:56:56 +0200

    Made the testrunner Django 1.2 compatible.

--- model_utils/tests/runtests.py
+++ model_utils/tests/runtests.py
@@ -8,12 +8,11 @@ parent = os.path.dirname(os.path.dirname(os.path.dirname(
 
 sys.path.insert(0, parent)
 
-from django.test.utils import get_runner
+from django.test.simple import run_tests
 from django.conf import settings
 
 def runtests():
-    test_runner = get_runner(settings)
-    failures = test_runner(['tests'], verbosity=1, interactive=True)
+    failures = run_tests(['tests'], verbosity=1, interactive=True)
     sys.exit(failures)
 
 if __name__ == '__main__':
