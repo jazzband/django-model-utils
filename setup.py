a3c838addafdb97c500561fd667a55ecf7268216
Carl Meyer <carl@oddbird.net>
2011-02-18 17:20:11 -0500

    Bumped version for dev.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ long_description = (open('README.rst').read() +
 
 setup(
     name='django-model-utils',
-    version='0.6.0',
+    version='0.6.0.post%s' % hgrev,
     description='Django model mixins and utilities',
     long_description=long_description,
     author='Carl Meyer',
