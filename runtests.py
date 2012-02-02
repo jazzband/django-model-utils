38dee4c560ecfdcb6aaca662e06aecbd60864ace
Carl Meyer <carl@oddbird.net>
2012-02-02 08:31:52 -0700

    Remove pre-1.2 test-runner support.

--- runtests.py
+++ runtests.py
@@ -29,16 +29,9 @@ def runtests(*test_args):
     parent = os.path.dirname(os.path.abspath(__file__))
     sys.path.insert(0, parent)
 
-    try:
-        from django.test.simple import DjangoTestSuiteRunner
-        def run_tests(test_args, verbosity, interactive):
-            runner = DjangoTestSuiteRunner(
-                verbosity=verbosity, interactive=interactive, failfast=False)
-            return runner.run_tests(test_args)
-    except ImportError:
-        # for Django versions that don't have DjangoTestSuiteRunner
-        from django.test.simple import run_tests
-    failures = run_tests(test_args, verbosity=1, interactive=True)
+    from django.test.simple import DjangoTestSuiteRunner
+    failures = DjangoTestSuiteRunner(
+        verbosity=1, interactive=True, failfast=False).run_tests(test_args)
     sys.exit(failures)
 
 
