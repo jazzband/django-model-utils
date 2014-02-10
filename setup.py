879eda8cd3b6d346b8b697d1c35e28892d830ef6
Travis Swicegood <development@domain51.com>
2014-02-10 14:35:11 -0600

    Fix Django dependency
    Django is packaged with a capital D.  Having a dependency on "django"
    will break any static indexes.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email='carl@oddbird.net',
     url='https://github.com/carljm/django-model-utils/',
     packages=find_packages(),
-    install_requires=['django>=1.4.2'],
+    install_requires=['Django>=1.4.2'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
