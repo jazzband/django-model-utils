dcd288cb09ebc2d973c0a5428418acd8b98e3427
jarekwg <jarekwg@gmail.com>
2015-10-29 09:42:16 +1100

    Jumped the gun with py35 tests..
    https://github.com/travis-ci/travis-ci/issues/4794

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Framework :: Django',
     ],
     zip_safe=False,
