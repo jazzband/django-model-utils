eb41dc0ea07ac40db447dd446410f115e9d2bb85
romgar <romain.garrigues.cs@gmail.com>
2016-11-24 21:31:55 +0000

    Prevent test manual import for Django 1.6+. No impact on Django 1.7/1.8, duplicating all test runs in Django 1.6

--- model_utils/tests/tests.py
+++ model_utils/tests/tests.py
@@ -1,6 +1,9 @@
+import django
+
 # Needed for Django 1.4/1.5 test runner
-from .test_fields import *
-from .test_managers import *
-from .test_models import *
-from .test_choices import *
-from .test_miscellaneous import *
+if django.VERSION < (1, 6):
+    from .test_fields import *
+    from .test_managers import *
+    from .test_models import *
+    from .test_choices import *
+    from .test_miscellaneous import *
