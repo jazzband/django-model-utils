f8cd3f6e07a3fa904218ac37f5a0ad9df69b45d0
H. Buğra Aydın <32219686+bugraaydin@users.noreply.github.com>
2020-01-21 10:55:07 +0300

    Overriding TimeStampedModel's save() method
    to update modified field automatically when update_fields argument is used but modified was not given as a parameter.

--- model_utils/models.py
+++ model_utils/models.py
@@ -28,6 +28,16 @@ class TimeStampedModel(models.Model):
     created = AutoCreatedField(_('created'))
     modified = AutoLastModifiedField(_('modified'))
 
+    def save(self, *args, **kwargs):
+        """
+        Overriding the save method in order to make sure that
+        modified field is updated even if it is not given as
+        a parameter to the update field argument.
+        """
+        if 'update_fields' in kwargs and 'modified' not in kwargs['update_fields']:
+            kwargs['update_fields'].append('modified')
+        super().save(*args, **kwargs)
+        
     class Meta:
         abstract = True
 
