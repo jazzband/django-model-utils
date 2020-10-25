92ff6b927ff5587c33528397d192b2c3ed0a4b4e
Hasan Ramezani <hasan.r67@gmail.com>
2020-10-25 19:53:01 +0100

    Remove codes related to old versions of Django.

--- tests/test_inheritance_iterable.py
+++ tests/test_inheritance_iterable.py
@@ -1,6 +1,3 @@
-from unittest import skipIf
-
-import django
 from django.test import TestCase
 from django.db.models import Prefetch
 
@@ -8,7 +5,6 @@ from tests.models import InheritanceManagerTestParent, InheritanceManagerTestChi
 
 
 class InheritanceIterableTest(TestCase):
-    @skipIf(django.VERSION[:2] == (1, 10), "Django 1.10 expects ModelIterable not a subclass of it")
     def test_prefetch(self):
         qs = InheritanceManagerTestChild1.objects.all().prefetch_related(
             Prefetch(
