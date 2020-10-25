34f3fe01b13d3400d6299c1e91adbee7017c5b0b
Hasan Ramezani <hasan.r67@gmail.com>
2020-10-25 15:59:32 +0100

    Replace ugettext_lazy with gettext_lazy in test models to satisfy Django deprecation warning.

--- tests/models.py
+++ tests/models.py
@@ -2,7 +2,7 @@ import django
 from django.db import models
 from django.db.models.query_utils import DeferredAttribute
 from django.db.models import Manager
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from model_utils import Choices
 from model_utils.fields import (
