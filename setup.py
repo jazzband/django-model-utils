d3c0aaa1ad8d353f7fe0fef6a32d2b4a6596c69b
Hasan Ramezani <hasan.r67@gmail.com>
2020-11-06 13:54:31 +0100

    Add Python3.9 support.

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Framework :: Django',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
