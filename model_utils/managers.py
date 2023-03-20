a8c20b695293f0a0f021ca91d048720b47c57c33
Maarten ter Huurne <maarten.terhuurne@protix.eu>
2023-03-20 19:16:03 +0100

    Annotate the `managers` module

--- model_utils/managers.py
+++ model_utils/managers.py
@@ -1,20 +1,36 @@
 from __future__ import annotations
 
 import warnings
+from typing import TYPE_CHECKING, Generic, TypeVar, cast
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import connection, models
 from django.db.models.constants import LOOKUP_SEP
 from django.db.models.fields.related import OneToOneField, OneToOneRel
-from django.db.models.query import ModelIterable, QuerySet
 from django.db.models.sql.datastructures import Join
+
+ModelT = TypeVar('ModelT', bound=models.Model, covariant=True)
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable, Iterator
+    from typing import Any
+
+    from django.db.models.query import BaseIterable
+    from django.db.models.query import ModelIterable as ModelIterableGeneric
+    from django.db.models.query import QuerySet as QuerySetGeneric
+    from django.db.models.sql.query import Query
+
+    ModelIterable = ModelIterableGeneric[ModelT]
+    QuerySet = QuerySetGeneric[ModelT]
+else:
+    from django.db.models.query import ModelIterable, QuerySet
 
 
 class InheritanceIterable(ModelIterable):
-    def __iter__(self):
+    def __iter__(self) -> Iterator[ModelT]:
         queryset = self.queryset
-        iter = ModelIterable(queryset)
-        if getattr(queryset, 'subclasses', False):
+        iter: Iterable[ModelT] = ModelIterable(queryset)
+        if hasattr(queryset, 'subclasses'):
             extras = tuple(queryset.query.extra.keys())
             # sort the subclass names longest first,
             # so with 'a' and 'a__b' it goes as deep as possible
@@ -22,13 +38,14 @@ class InheritanceIterable(ModelIterable):
             for obj in iter:
                 sub_obj = None
                 for s in subclasses:
+                    assert hasattr(queryset, '_get_sub_obj_recurse')
                     sub_obj = queryset._get_sub_obj_recurse(obj, s)
                     if sub_obj:
                         break
                 if not sub_obj:
                     sub_obj = obj
 
-                if getattr(queryset, '_annotated', False):
+                if hasattr(queryset, '_annotated'):
                     for k in queryset._annotated:
                         setattr(sub_obj, k, getattr(obj, k))
 
@@ -40,28 +57,33 @@ class InheritanceIterable(ModelIterable):
             yield from iter
 
 
-class InheritanceQuerySetMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._iterable_class = InheritanceIterable
+class InheritanceQuerySetMixin(Generic[ModelT]):
 
-    def select_subclasses(self, *subclasses):
+    # Defining an attribute using a generic type will trigger a bug in mypy:
+    # https://github.com/python/mypy/issues/9031
+    # model: type[ModelT]
+
+    def __init__(self, *args: object, **kwargs: object):
+        super().__init__(*args, **kwargs)
+        self._iterable_class: type[BaseIterable] = InheritanceIterable
+
+    def select_subclasses(self, *subclasses: str | type[models.Model]) -> InheritanceQuerySet[ModelT]:
         levels = None
-        calculated_subclasses = self._get_subclasses_recurse(
-            self.model, levels=levels)
+        model: type[ModelT] = self.model  # type: ignore[attr-defined]
+        calculated_subclasses = self._get_subclasses_recurse(model, levels=levels)
         # if none were passed in, we can just short circuit and select all
         if not subclasses:
-            subclasses = calculated_subclasses
+            selected_subclasses = calculated_subclasses
         else:
-            verified_subclasses = []
+            verified_subclasses: list[str] = []
             for subclass in subclasses:
                 # special case for passing in the same model as the queryset
                 # is bound against. Rather than raise an error later, we know
                 # we can allow this through.
-                if subclass is self.model:
+                if subclass is model:
                     continue
 
-                if not isinstance(subclass, (str,)):
+                if not isinstance(subclass, str):
                     subclass = self._get_ancestors_path(
                         subclass, levels=levels)
 
@@ -72,38 +94,43 @@ class InheritanceQuerySetMixin:
                         '{!r} is not in the discovered subclasses, tried: {}'.format(
                             subclass, ', '.join(calculated_subclasses))
                     )
-            subclasses = verified_subclasses
+            selected_subclasses = verified_subclasses
 
-        if subclasses:
-            new_qs = self.select_related(*subclasses)
-        else:
-            new_qs = self
-        new_qs.subclasses = subclasses
+        new_qs = cast('InheritanceQuerySet[ModelT]', self)
+        if selected_subclasses:
+            new_qs = new_qs.select_related(*selected_subclasses)
+        new_qs.subclasses = selected_subclasses  # type: ignore[attr-defined]
         return new_qs
 
-    def _chain(self, **kwargs):
+    def _chain(self, **kwargs: object) -> InheritanceQuerySet[ModelT]:
         update = {}
         for name in ['subclasses', '_annotated']:
             if hasattr(self, name):
                 update[name] = getattr(self, name)
 
-        chained = super()._chain(**kwargs)
+        # django-stubs doesn't include private API.
+        chained = super()._chain(**kwargs)  # type: ignore[misc]
         chained.__dict__.update(update)
         return chained
 
-    def _clone(self):
-        qs = super()._clone()
+    def _clone(self) -> InheritanceQuerySet[ModelT]:
+        # django-stubs doesn't include private API.
+        qs = super()._clone()  # type: ignore[misc]
         for name in ['subclasses', '_annotated']:
             if hasattr(self, name):
                 setattr(qs, name, getattr(self, name))
         return qs
 
-    def annotate(self, *args, **kwargs):
-        qset = super().annotate(*args, **kwargs)
+    def annotate(self, *args: Any, **kwargs: Any) -> InheritanceQuerySet[ModelT]:
+        qset = cast(QuerySet, super()).annotate(*args, **kwargs)
         qset._annotated = [a.default_alias for a in args] + list(kwargs.keys())
         return qset
 
-    def _get_subclasses_recurse(self, model, levels=None):
+    def _get_subclasses_recurse(
+        self,
+        model: type[models.Model],
+        levels: int | None = None
+    ) -> list[str]:
         """
         Given a Model class, find all related objects, exploring children
         recursively, returning a `list` of strings representing the
@@ -128,38 +155,47 @@ class InheritanceQuerySetMixin:
             if levels or levels is None:
                 for subclass in self._get_subclasses_recurse(
                         rel.field.model, levels=levels):
-                    subclasses.append(
-                        rel.get_accessor_name() + LOOKUP_SEP + subclass)
-            subclasses.append(rel.get_accessor_name())
+                    accessor_name = rel.get_accessor_name()
+                    assert accessor_name is not None
+                    subclasses.append(accessor_name + LOOKUP_SEP + subclass)
+            accessor_name = rel.get_accessor_name()
+            assert accessor_name is not None
+            subclasses.append(accessor_name)
         return subclasses
 
-    def _get_ancestors_path(self, model, levels=None):
+    def _get_ancestors_path(
+        self,
+        model: type[models.Model],
+        levels: int | None = None
+    ) -> str:
         """
         Serves as an opposite to _get_subclasses_recurse, instead walking from
         the Model class up the Model's ancestry and constructing the desired
         select_related string backwards.
         """
-        if not issubclass(model, self.model):
+        ancestor: type[ModelT] = self.model  # type: ignore[attr-defined]
+        if not issubclass(model, ancestor):
             raise ValueError(
-                f"{model!r} is not a subclass of {self.model!r}")
+                f"{model!r} is not a subclass of {ancestor!r}")
 
-        ancestry = []
+        ancestry: list[str] = []
         # should be a OneToOneField or None
-        parent_link = model._meta.get_ancestor_link(self.model)
+        parent_link = model._meta.get_ancestor_link(ancestor)
         if levels:
             levels -= 1
         while parent_link is not None:
             related = parent_link.remote_field
-            ancestry.insert(0, related.get_accessor_name())
+            accessor_name = related.get_accessor_name()
+            assert accessor_name is not None
+            ancestry.insert(0, accessor_name)
             if levels or levels is None:
                 parent_model = related.model
-                parent_link = parent_model._meta.get_ancestor_link(
-                    self.model)
+                parent_link = parent_model._meta.get_ancestor_link(ancestor)
             else:
                 parent_link = None
         return LOOKUP_SEP.join(ancestry)
 
-    def _get_sub_obj_recurse(self, obj, s):
+    def _get_sub_obj_recurse(self, obj: models.Model, s: str) -> ModelT | None:
         rel, _, s = s.partition(LOOKUP_SEP)
 
         try:
@@ -172,12 +208,12 @@ class InheritanceQuerySetMixin:
         else:
             return node
 
-    def get_subclass(self, *args, **kwargs):
+    def get_subclass(self, *args: object, **kwargs: object) -> ModelT:
         return self.select_subclasses().get(*args, **kwargs)
 
 
-class InheritanceQuerySet(InheritanceQuerySetMixin, QuerySet):
-    def instance_of(self, *models):
+class InheritanceQuerySet(InheritanceQuerySetMixin[ModelT], QuerySet[ModelT]):
+    def instance_of(self, *models: type[ModelT]) -> InheritanceQuerySet[ModelT]:
         """
         Fetch only objects that are instances of the provided model(s).
         """
@@ -200,88 +236,111 @@ class InheritanceQuerySet(InheritanceQuerySetMixin, QuerySet):
                 ) for field in model._meta.parents.values()
             ]) + ')')
 
-        return self.select_subclasses(*models).extra(where=[' OR '.join(where_queries)])
+        return cast(
+            'InheritanceQuerySet[ModelT]',
+            self.select_subclasses(*models).extra(where=[' OR '.join(where_queries)])
+        )
 
 
-class InheritanceManagerMixin:
+class InheritanceManagerMixin(Generic[ModelT]):
     _queryset_class = InheritanceQuerySet
 
-    def get_queryset(self):
-        return self._queryset_class(self.model)
+    if TYPE_CHECKING:
+        def filter(self, *args: Any, **kwargs: Any) -> InheritanceQuerySet[ModelT]:
+            ...
 
-    def select_subclasses(self, *subclasses):
+    def get_queryset(self) -> InheritanceQuerySet[ModelT]:
+        model: type[ModelT] = self.model  # type: ignore[attr-defined]
+        return self._queryset_class(model)
+
+    def select_subclasses(
+        self, *subclasses: str | type[models.Model]
+    ) -> InheritanceQuerySet[ModelT]:
         return self.get_queryset().select_subclasses(*subclasses)
 
-    def get_subclass(self, *args, **kwargs):
+    def get_subclass(self, *args: object, **kwargs: object) -> ModelT:
         return self.get_queryset().get_subclass(*args, **kwargs)
 
-    def instance_of(self, *models):
+    def instance_of(self, *models: type[ModelT]) -> QuerySet:
         return self.get_queryset().instance_of(*models)
 
 
-class InheritanceManager(InheritanceManagerMixin, models.Manager):
+class InheritanceManager(InheritanceManagerMixin[ModelT], models.Manager[ModelT]):
     pass
 
 
-class QueryManagerMixin:
+class QueryManagerMixin(Generic[ModelT]):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: models.Q, **kwargs: object):
         if args:
             self._q = args[0]
         else:
             self._q = models.Q(**kwargs)
-        self._order_by = None
+        self._order_by: tuple[Any, ...] | None = None
         super().__init__()
 
-    def order_by(self, *args):
+    # Using a generic type as the return type will trigger a bug in mypy:
+    # https://github.com/python/mypy/issues/9031
+    # def order_by(self, *args: Any) -> QueryManager[ModelT]:
+    def order_by(self, *args: Any) -> Any:
         self._order_by = args
-        return self
+        return cast('QueryManager[ModelT]', self)
 
-    def get_queryset(self):
-        qs = super().get_queryset().filter(self._q)
+    def get_queryset(self) -> QuerySet[ModelT]:
+        qs = super().get_queryset()  # type: ignore[misc]
+        qs = qs.filter(self._q)
         if self._order_by is not None:
             return qs.order_by(*self._order_by)
         return qs
 
 
-class QueryManager(QueryManagerMixin, models.Manager):
+class QueryManager(QueryManagerMixin[ModelT], models.Manager):
     pass
 
 
-class SoftDeletableQuerySetMixin:
+class SoftDeletableQuerySetMixin(Generic[ModelT]):
     """
-    QuerySet for SoftDeletableModel. Instead of removing instance sets
+f    QuerySet for SoftDeletableModel. Instead of removing instance sets
     its ``is_removed`` field to True.
     """
 
-    def delete(self):
+    def delete(self) -> tuple[int, dict[str, int]]:
         """
         Soft delete objects from queryset (set their ``is_removed``
         field to True)
         """
-        self.update(is_removed=True)
+        cast(QuerySet, self).update(is_removed=True)
         return (0, {})  # no objects were actually deleted
 
 
-class SoftDeletableQuerySet(SoftDeletableQuerySetMixin, QuerySet):
+class SoftDeletableQuerySet(SoftDeletableQuerySetMixin[ModelT], QuerySet):
     pass
 
 
-class SoftDeletableManagerMixin:
+class SoftDeletableManagerMixin(Generic[ModelT]):
     """
     Manager that limits the queryset by default to show only not removed
     instances of model.
     """
     _queryset_class = SoftDeletableQuerySet
 
-    def __init__(self, *args, _emit_deprecation_warnings=False, **kwargs):
+    _db: str | None
+
+    def __init__(
+        self,
+        *args: object,
+        _emit_deprecation_warnings: bool = False,
+        **kwargs: object
+    ):
         self.emit_deprecation_warnings = _emit_deprecation_warnings
         super().__init__(*args, **kwargs)
 
-    def get_queryset(self):
+    def get_queryset(self) -> SoftDeletableQuerySet[ModelT]:
         """
         Return queryset limited to not removed entries.
         """
+
+        model: type[ModelT] = self.model  # type: ignore[attr-defined]
 
         if self.emit_deprecation_warnings:
             warning_message = (
@@ -290,38 +349,35 @@ class SoftDeletableManagerMixin:
                 "excluding soft-deleted objects. See "
                 "https://django-model-utils.readthedocs.io/en/stable/models.html"
                 "#softdeletablemodel for more information."
-            ).format(self.model.__class__.__name__)
+            ).format(model.__class__.__name__)
             warnings.warn(warning_message, DeprecationWarning)
 
-        kwargs = {'model': self.model, 'using': self._db}
-        if hasattr(self, '_hints'):
-            kwargs['hints'] = self._hints
+        return self._queryset_class(
+            model=model,
+            using=self._db,
+            **({'hints': self._hints} if hasattr(self, '_hints') else {})
+        ).filter(is_removed=False)
 
-        return self._queryset_class(**kwargs).filter(is_removed=False)
 
-
-class SoftDeletableManager(SoftDeletableManagerMixin, models.Manager):
+class SoftDeletableManager(SoftDeletableManagerMixin[ModelT], models.Manager):
     pass
 
 
 class JoinQueryset(models.QuerySet):
 
-    def get_quoted_query(self, query):
-        query, params = query.sql_with_params()
+    def get_quoted_query(self, query: Query) -> str:
+        query_str, params = query.sql_with_params()
 
         # Put additional quotes around string.
-        params = [
+        params = tuple(
             f'\'{p}\''
             if isinstance(p, str) else p
             for p in params
-        ]
+        )
 
-        # Cast list of parameters to tuple because I got
-        # "not enough format characters" otherwise.
-        params = tuple(params)
-        return query % params
+        return query_str % params
 
-    def join(self, qs=None):
+    def join(self, qs: QuerySet | None = None) -> QuerySet:
         '''
         Join one queryset together with another using a temporary table. If
         no queryset is used, it will use the current queryset and join that
@@ -336,11 +392,11 @@ class JoinQueryset(models.QuerySet):
         to_field = 'id'
 
         if qs:
-            fk = [
+            fks = [
                 fk for fk in qs.model._meta.fields
                 if getattr(fk, 'related_model', None) == self.model
             ]
-            fk = fk[0] if fk else None
+            fk = fks[0] if fks else None
             model_set = f'{self.model.__name__.lower()}_set'
             key = fk or getattr(qs.model, model_set, None)
 
@@ -397,16 +453,19 @@ class JoinQueryset(models.QuerySet):
         return new_qs
 
 
-class JoinManagerMixin:
+class JoinManagerMixin(Generic[ModelT]):
     """
     Manager that adds a method join. This method allows you to join two
     querysets together.
     """
     _queryset_class = JoinQueryset
 
-    def get_queryset(self):
-        return self._queryset_class(model=self.model, using=self._db)
+    _db: str | None
+
+    def get_queryset(self) -> JoinQueryset:
+        model: type[ModelT] = self.model  # type: ignore[attr-defined]
+        return self._queryset_class(model=model, using=self._db)
 
 
-class JoinManager(JoinManagerMixin, models.Manager):
+class JoinManager(JoinManagerMixin[ModelT], models.Manager):
     pass
