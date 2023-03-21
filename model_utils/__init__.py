00e52e85089aecd80b79554f36e870a0b4701c89
Maarten ter Huurne <maarten.terhuurne@protix.eu>
2023-03-21 14:25:33 +0100

    Explicitly re-export names from `__init__` module
    Both mypy and flake8 understand `__all__`, removing the need for
    warning suppression comments.

--- model_utils/__init__.py
+++ model_utils/__init__.py
@@ -1,10 +1,12 @@
 import importlib.metadata
 
-from .choices import Choices  # noqa:F401
-from .tracker import FieldTracker, ModelTracker  # noqa:F401
+from .choices import Choices
+from .tracker import FieldTracker, ModelTracker
 
 try:
     __version__ = importlib.metadata.version('django-model-utils')
 except importlib.metadata.PackageNotFoundError:  # pragma: no cover
     # package is not installed
     __version__ = None
+
+__all__ = ("Choices", "FieldTracker", "ModelTracker")
