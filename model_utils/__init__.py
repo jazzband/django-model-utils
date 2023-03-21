37cfcceb0e95b603cccbd9d55619ed9f85889284
Maarten ter Huurne <maarten.terhuurne@protix.eu>
2023-03-21 14:26:11 +0100

    Suppress error about `__version__` being `None` when not installed
    Suppress rather than annotate, because people type checking against
    `django-model-utils` will always have it installed and therefore
    should not have to deal with `__version__` being `None`.

--- model_utils/__init__.py
+++ model_utils/__init__.py
@@ -7,6 +7,6 @@ try:
     __version__ = importlib.metadata.version('django-model-utils')
 except importlib.metadata.PackageNotFoundError:  # pragma: no cover
     # package is not installed
-    __version__ = None
+    __version__ = None  # type: ignore[assignment]
 
 __all__ = ("Choices", "FieldTracker", "ModelTracker")
