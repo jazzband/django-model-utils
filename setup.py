0476fad22a8e9d8e7f3bc25a778d81c7063edd50
jelmert <info@jelmert.nl>
2024-08-28 09:48:40 +0200

    Add formal support for django 5.1

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'Framework :: Django :: 4.1',
         'Framework :: Django :: 4.2',
         'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
     ],
     zip_safe=False,
     package_data={
